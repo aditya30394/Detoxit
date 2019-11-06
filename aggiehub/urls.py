a81cc58b9342b264ca52f17ca0160f305f7d9db5
Aditya Kumar <aditya30394@gmail.com>
2019-11-05 20:34:53 -0600

    login/logout implemented

--- aggiehub/urls.py
+++ aggiehub/urls.py
@@ -6,6 +6,7 @@ from aggiehub import views
 urlpatterns = [
     path("", views.login, name="login"),
     path("home/", views.home, name="home"),
+    path("logout/", views.logout, name="logout"),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- aggiehub/views.py
+++ aggiehub/views.py
@@ -4,21 +4,33 @@ from django.shortcuts import redirect, render
 from aggiehub.forms import LoginForm
 from aggiehub.models import User
 
+user = None
+
 
 def home(request):
-    return HttpResponse("Hello, Django!")
+    if user is None:
+        return redirect("login")
+    return render(request, "aggiehub/home.html", {"user": user})
 
 
 def login(request):
     form = LoginForm(request.POST or None)
-
+    global user
+    user = None
     if request.method == "POST":
         if form.is_valid():
             email = form.save(commit=False).email
             try:
                 user = User.objects.get(email=email)
             except User.DoesNotExist:
-                return render(request, "aggiehub/login.html", {"form": LoginForm(None)})
+                context = {"form": LoginForm(None), "user": user}
+                return render(request, "aggiehub/login.html", context)
             return redirect("home")
     else:
-        return render(request, "aggiehub/login.html", {"form": form})
+        context = {"form": form, "user": user}
+        return render(request, "aggiehub/login.html", context)
+
+
+def logout(request):
+    user = None
+    return redirect("login")
