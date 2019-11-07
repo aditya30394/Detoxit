529fcdd58e68b91c027b7c8818cb1c5bf5e19efb
Aditya Kumar <aditya30394@gmail.com>
2019-11-06 23:43:55 -0600

    Post from UI

--- aggiehub/forms.py
+++ aggiehub/forms.py
@@ -1,8 +1,13 @@
 from django import forms
-from aggiehub.models import User
+from aggiehub.models import User, Post
 
 
 class LoginForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ("email", )
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ("text", )
--- aggiehub/views.py
+++ aggiehub/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 
-from aggiehub.forms import LoginForm
+from aggiehub.forms import LoginForm, PostForm
 from aggiehub.models import User
 
 user = None
@@ -10,8 +10,18 @@ user = None
 def home(request):
     if user is None:
         return redirect("login")
-    posts = user.post_set.order_by('-id')
-    return render(request, "aggiehub/home.html", {"user": user, "posts": posts})
+        
+    form = PostForm(request.POST or None)
+    if request.method == "POST":
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = user
+            post.save()
+            return redirect("home")
+    else:
+        posts = user.post_set.order_by('-id')
+        context = {"form": form, "user": user, "posts": posts}
+        return render(request, "aggiehub/home.html", context)
 
 
 def login(request):
