169ad790e2ccd914673874c6b85af4e4d16722d6
Aditya Kumar <aditya30394@gmail.com>
2019-12-01 01:13:09 -0600

    Added a new survey form

--- aggiehub/forms.py
+++ aggiehub/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from aggiehub.models import User, Post
-
+from django.forms import widgets
 
 class LoginForm(forms.ModelForm):
     class Meta:
@@ -11,3 +11,9 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ("text", )
+
+class SurveyForm(forms.Form):
+    post_text = forms.CharField(label="Post", disabled=True)
+    SCORE_CHOICES = zip(range(1, 6), range(1, 6))
+    score = forms.ChoiceField(label="Score",choices=SCORE_CHOICES, widget=forms.RadioSelect)
+    
